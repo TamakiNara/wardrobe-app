d107b39bb4e4fa58449b198d14115b3af4c3096f
TamakiNara <tamaki97.dev@gmail.com>
2026-03-08 12:48:04 +0900

    style(web): apply prettier formatting

--- web/next.config.ts
+++ web/next.config.ts
@@ -2,14 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-async rewrites() {
+  async rewrites() {
     return [
-          {
-              source:"/api/:path*",
-              destination:"http://localhost:8000/api/:path*",
-          },
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:8000/api/:path*",
+      },
     ];
-}
+  },
 };
 
 export default nextConfig;
